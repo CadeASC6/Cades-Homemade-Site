15212c3b72bbc37805c08b703dd7c4ce7d2f99c0
CadeASC6 <52716010+CadeASC6@users.noreply.github.com>
2019-08-19 12:58:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// need to work out how to make dropdown menu
+// workout a tag onclicks
+//hide everthing to start
+//more link??
+// work out hover functionnality
+//color of each section should change if your in it
